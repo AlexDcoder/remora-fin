bda15a3ebc09a1ffd85469884051a5e6e9c9915c
Alex <alexandrepinto586@gmail.com>
2026-06-26 12:54:06 -0300

    fix: Reformating code for ruff conditions

--- src/remora_fin/services/inventory_service.py
+++ src/remora_fin/services/inventory_service.py
@@ -142,15 +142,15 @@ RESOURCE_REGISTRY: dict[str, ResourceConfig] = {
         mapper=lambda x: {"name": x},
     ),
     "vpc": ResourceConfig(
-    client_name="ec2",
-    paginator_name="describe_vpcs",
-    result_path=["Vpcs"],
-    mapper=lambda x: {
-        "id": x["VpcId"],
-        "cidr": x["CidrBlock"],
-        "state": x["State"],
-        "is_default": x["IsDefault"],
-    },
+        client_name="ec2",
+        paginator_name="describe_vpcs",
+        result_path=["Vpcs"],
+        mapper=lambda x: {
+            "id": x["VpcId"],
+            "cidr": x["CidrBlock"],
+            "state": x["State"],
+            "is_default": x["IsDefault"],
+        },
     ),
 }
 
